60f83ea5661be6f6764435bddabe68d082ae5e9f
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-08-07 08:29:00 -0700

    installed dependencies

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+
+import { useState} from React;
+
+export const useForm =() => {
+    const [value, setValue] = useState('')
+    const handleChanges = e =>{
+        setValue(e.target.value)
+    }
+    const clearForm = e => {
+        e.preventDefault()
+        setValue('')
+    }
+    return [value, setValue, handleChanges, clearForm]
+}
+
